72909f0e492e854c35e68b32b16df72ef49bd232
moreal <dev.moreal@gmail.com>
2019-01-03 17:30:56 +0900

    Update all models

--- backend/gohome/models.py
+++ backend/gohome/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django_countries.fields import CountryField
 
 class School(models.Model):
+    name = models.CharField(max_length=100, default='')
     site = models.CharField(max_length=2083)
     country = CountryField()
 
 class HomeComingDay(models.Model):
-    school_id = models.ForeignKey(to="School", on_delete=models.DO_NOTHING)
+    school_id = models.ForeignKey(School, on_delete=models.DO_NOTHING)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()

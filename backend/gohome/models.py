fcf6368f2187e1548ef901ccec7431017bcd1f7b
moreal <dev.moreal@gmail.com>
2019-01-03 17:17:03 +0900

    Make backend models

---
+++ backend/gohome/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django_countires.fields import CountryField
+
+class School(models.Model):
+    site = models.CharField(max_length=2083)
+    country = CountryField()
+
+class HomeComingDay(models.Model):
+    school_id = models.ForeignKey(to="School", on_delete=models.DO_NOTHING)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()

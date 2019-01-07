351af7149624ef07cae095255ef27faaaef47346
moreal <dev.moreal@gmail.com>
2019-01-07 14:38:44 +0900

    Add this stage for frontend, yarn eject

---
+++ front/src/components/timer/school/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "school.scss";
+
+const SchoolComponent = () => (
+    <span id='school-name'>{this.props.name}</span>
+)
+
+export default SchoolComponent;

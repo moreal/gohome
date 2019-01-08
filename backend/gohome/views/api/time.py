e55b45f940db7aa4c6d8b421adddabf4d6a4ffbb
moreal <dev.moreal@gmail.com>
2019-01-08 12:27:29 +0900

    Add api/time case for test

--- backend/gohome/views/api/time.py
+++ backend/gohome/views/api/time.py
@@ -1,8 +1,12 @@
 from django.views import View
 from django.http import HttpRequest, HttpResponse
+import json, time
 
 
 class TimeView(View):
 
     def get(self, request: HttpRequest, *args, **kwargs):
-        return HttpResponse("{'time':'test'}")
+        return HttpResponse(json.dumps({
+            'time': time.time(),
+            'school_name': '대마고앙조아용',
+        }))

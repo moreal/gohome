c9de494393af4848686a2fef9c15f89f56dbeb5a
moreal <dev.moreal@gmail.com>
2019-01-10 15:31:08 +0900

    Complete to write api/time

--- backend/gohome/views/api/time.py
+++ backend/gohome/views/api/time.py
@@ -1,12 +1,17 @@
 from django.views import View
 from django.http import HttpRequest, HttpResponse
-import json, time
+import json
+import time
+from gohome.models import HomeComingDay
 
 
 class TimeView(View):
 
-    def get(self, request: HttpRequest, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
+        school_name = request.COOKIES.get('school_name', '대덕소프트웨어마이스터고등학교')
+        day = HomeComingDay.objects.select_related('school').get(school__name=school_name)
+
         return HttpResponse(json.dumps({
-            'time': time.time(),
-            'school_name': '대마고앙조아용',
+            'time': int(day.end_datetime.timestamp()) * 1000,
+            'school_name': day.school.name,
         }))
